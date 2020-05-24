bcdd5547c36fba085fa865bcdc9c4661e0f1a056
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-24 23:46:56 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,5 +1,6 @@
 class School
 
-school = School.new("Bayside High School")
-school.add_student("Zach Morris", 9)
-school.roster
+def initialize(school)
+  @school_name = school_name
+  @roster = {}
+end
